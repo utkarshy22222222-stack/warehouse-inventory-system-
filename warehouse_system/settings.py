81b6cfcce3a6f1400dfe53441fe0b665dbc3ae36
utkarshy22222222-stack <utkarshy22222222@gmail.com>
2026-07-11 23:26:50 +0530

    Add STATIC_ROOT for collectstatic on Render

--- warehouse_system/settings.py
+++ warehouse_system/settings.py
@@ -128,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
